3fd1658b9140f4349994a765cdbeca192392f96d
Eliot Kent Woodrich <ekwoodrich@gmail.com>
2019-08-05 20:46:01 -0700

    Updated gitingore

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,16 +1,6 @@
 import * as firebase from 'firebase'  // Should not be used elsewhere in the project
+import {firebaseConfig} from '../secrets'
 
-
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBqd4g56-Ya973zXdd7eb9ECvdz2p10HV0",
-  authDomain: "thinkwith-b4627.firebaseapp.com",
-  databaseURL: "https://thinkwith-b4627.firebaseio.com",
-  projectId: "thinkwith-b4627",
-  storageBucket: "",
-  messagingSenderId: "892887036275",
-  appId: "1:892887036275:web:81149189a993c08a"
-};
 
 firebase.initializeApp(firebaseConfig);
 
