91c5e86bd2bb86037e77e2c534c6fa31172e4728
Eliot Kent Woodrich <ekwoodrich@gmail.com>
2019-08-05 20:54:19 -0700

    Fixed secrets import.

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,7 +1,8 @@
 import * as firebase from 'firebase'  // Should not be used elsewhere in the project
-import {firebaseConfig} from '../secrets'
+import firebaseConfig from '../secrets'
 
 
+console.log(firebaseConfig)
 firebase.initializeApp(firebaseConfig);
 
 export default firebase;
