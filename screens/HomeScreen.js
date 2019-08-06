1a03b2fb6941c3e4649aff74aa76b7a6b56d77d7
Eliot Kent Woodrich <ekwoodrich@gmail.com>
2019-08-05 21:27:45 -0700

    Added appbar to navigate note history.

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,29 +1,49 @@
-import React from 'react';
-import { Button, View, Text } from 'react-native';
-import { createStackNavigator, createAppContainer } from 'react-navigation';
-import NoteCard from '../components/NoteCard';
-import NewNote from '../components/NewNote';
-import { thinkorange, thinkblack} from '../defs/thinkcolor';
+import React from "react";
+import { Button, View, Text } from "react-native";
+import { createStackNavigator, createAppContainer } from "react-navigation";
+import NoteCard from "../components/NoteCard";
+import NewNote from "../components/NewNote";
+import { thinkorange, thinkblack } from "../defs/thinkcolor";
+import { Appbar } from "react-native-paper";
+import { StyleSheet } from "react-native";
 
+const styles = StyleSheet.create({
+  bottom: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    top: 0
+  }
+});
 class HomeScreen extends React.Component {
-      static navigationOptions = {
-        header: null
-    }
+  static navigationOptions = {
+    header: null
+  };
 
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-        <Button
-          title="Go to Profile"
-          onPress={() => this.props.navigation.navigate('Profile')}
-        />
-        <Button
-          title="Go to Calendar"
-          onPress={() => this.props.navigation.navigate('Calendar')}
-        />
-        <NewNote onPress={() => this.props.navigation.navigate('NewNote')} />
-      </View>
+      <>
+        <Appbar.Header>
+          <Appbar.Content title="Today" subtitle="August 8th, 2019" />
+          <Appbar.Action icon="chevron-left" onPress={this._onPrev} />
+          <Appbar.Action icon="date-range" onPress={this._onCal} />
+          <Appbar.Action icon="chevron-right" onPress={this._onNext} />
+        </Appbar.Header>
+        <View
+          style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+        >
+          <Text>Home Screen</Text>
+          <Button
+            title="Go to Profile"
+            onPress={() => this.props.navigation.navigate("Profile")}
+          />
+          <Button
+            title="Go to Calendar"
+            onPress={() => this.props.navigation.navigate("Calendar")}
+          />
+          <NewNote onPress={() => this.props.navigation.navigate("NewNote")} />
+        </View>
+      </>
     );
   }
 }
