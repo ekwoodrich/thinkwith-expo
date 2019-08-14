3f5efca7889f9ce900d0892662a15617c7e0a7e3
Eliot Kent Woodrich <ekwoodrich@gmail.com>
2019-08-13 23:38:57 -0700

    ActivityIndicator no longer overlaps NoNote message

--- components/NoteDay.js
+++ components/NoteDay.js
@@ -154,7 +154,7 @@ const styles = StyleSheet.create({
     position: "absolute",
     left: 0,
     right: 0,
-    top: 70,
+    top: 120,
     bottom: 0,
     opacity: 0.5,
     backgroundColor: "black",
